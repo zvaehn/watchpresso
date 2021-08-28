d5853d084bf01cf887cb3e94589ada38b6cb1bfa
Sven Schiffer <svenschiffer92@gmail.com>
2021-08-28 14:17:47 +0200

    deploy

---
+++ precache-manifest.d85c1e7c5a544d947946eb6ac5f4dc73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba325fcdc689349230d4",
+    "url": "/watchpresso/css/app.141d426a.css"
+  },
+  {
+    "revision": "af59954ed5499e7b88ec",
+    "url": "/watchpresso/css/chunk-vendors.259693f9.css"
+  },
+  {
+    "revision": "c95a6ae104cb3b43e42b4820fd412dec",
+    "url": "/watchpresso/index.html"
+  },
+  {
+    "revision": "ba325fcdc689349230d4",
+    "url": "/watchpresso/js/app.0c276c99.js"
+  },
+  {
+    "revision": "af59954ed5499e7b88ec",
+    "url": "/watchpresso/js/chunk-vendors.495ebb40.js"
+  },
+  {
+    "revision": "3c86c835bde7add17d2c8d7445373356",
+    "url": "/watchpresso/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/watchpresso/robots.txt"
+  }
+]);
